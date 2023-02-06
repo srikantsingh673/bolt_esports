9d49d93d3a12416da62df2f839b05380037452f3
srikantsingh673 <srikantsingh673@gmail.com>
2023-02-07 00:40:17 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
       <Container fluid="md">
       <BrowserRouter>
         <Routes>
-        <Route path='/web' element={<Header/>} />
-          <Route path='/web/teams' element={<Teams />} />
-          <Route path='/web/registration' element={<Registration />} />
+        <Route path='/' element={<Header/>} />
+          <Route path='teams' element={<Teams />} />
+          <Route path='registration' element={<Registration />} />
         </Routes>
       </BrowserRouter>
       </Container>
